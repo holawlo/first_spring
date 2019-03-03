ea0845b52fff5f21480ba6b2eca0bb11d3736abf
holawlo <holawlo@gmail.com>
2019-03-03 20:13:12 +0100

    beginning

---
+++ src/main/java/trials/MainTrial.java
@@ -0,0 +1,13 @@
+package trials;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainTrial {
+    public static void main(String[] args) {
+        SpringApplication.run(MainTrial.class, args);
+    }
+
+}
